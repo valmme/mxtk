8c75a5448df7c1f5861685a531ee48316c65e424
valmme <vagif.fb12@gmail.com>
2025-09-10 16:23:31 +0400

    Basic window

--- Program.cs
+++ Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Raylib_cs;
+using static Raylib_cs.Raylib;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+    
+    static void Main()
+    {
+        if (!Directory.Exists("albums")) Directory.CreateDirectory("albums");
+        
+        InitWindow(screenWidth, screenHeight, "mxtk");
+        SetTargetFPS(60);
+
+        while (!WindowShouldClose())
+        {
+            BeginDrawing();
+            ClearBackground(Color.DarkGray);
+            
+            EndDrawing();
+        }
+        
+        CloseWindow();
+    }
+}
