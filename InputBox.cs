541e959f35f20119d09d8bf040253104b61b6c65
valmme <vagif.fb12@gmail.com>
2025-09-10 23:04:22 +0400

    input box

---
+++ InputBox.cs
@@ -0,0 +1,111 @@
+using System.Numerics;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+
+namespace mxtk;
+
+public class InputBox
+{
+    private char[] name;
+    private int maxInputChars;
+    private int letterCount = 0;
+    private int framesCounter;
+    private bool mouseOn = false;
+    private Rectangle textBox;
+
+    public InputBox(int maxChars, Rectangle box)
+    {
+        maxInputChars = maxChars;
+        name = new char[maxInputChars];
+        textBox = box;
+    }
+
+    public void Draw(Resources tex)
+    {
+        if (IsMouseButtonPressed(MouseButton.Left))
+        {
+            if (CheckCollisionPointRec(GetMousePosition(), textBox)) mouseOn = true;
+            else mouseOn = false;
+        }
+        
+        if (mouseOn)
+        {
+
+            SetMouseCursor(MouseCursor.IBeam);
+            int key = GetCharPressed();
+
+            while (key > 0)
+            {
+                if ((key >= 32) && (key <= 255) && (letterCount < maxInputChars))
+                {
+                    name[letterCount] = (char)key;
+                    letterCount++;
+                }
+
+                key = GetCharPressed();
+            }
+
+            if (IsKeyPressed(KeyboardKey.Backspace))
+            {
+                letterCount -= 1;
+                if (letterCount < 0) letterCount = 0;
+                name[letterCount] = '\0';
+            }
+        }
+        
+        else SetMouseCursor(MouseCursor.Default);
+
+        if (mouseOn) framesCounter += 1;
+        else framesCounter = 0;
+        
+        DrawRectangleRec(textBox, new Color(200, 200, 200, 255));
+        
+        if (mouseOn)
+        {
+            DrawRectangleLinesEx(
+                new Rectangle(
+                    (int)textBox.X,
+                    (int)textBox.Y,
+                    (int)textBox.Width,
+                    (int)textBox.Height),
+                2,
+                new Color(70, 70, 70, 255)
+            );
+        }
+        
+        else
+        {
+            DrawRectangleLinesEx(
+                new Rectangle(
+                    (int)textBox.X,
+                    (int)textBox.Y,
+                    (int)textBox.Width,
+                    (int)textBox.Height),
+                2,
+                Color.Gray
+            );
+        }
+        
+        DrawTextEx(tex.font, new string(name), new Vector2((int)textBox.X + 5, (int)textBox.Y + 6), 25, 1, Color.DarkGray);
+
+        
+        if (mouseOn)
+        {
+            if (letterCount < maxInputChars)
+            {
+                if ((framesCounter / 20 % 2) == 0)
+                {
+                    DrawTextEx(tex.font,
+                        "_",
+                        new Vector2((int)textBox.X + 8 + MeasureText(new string(name), 25),
+                        (int)textBox.Y + 7),
+                        25,
+                        1,
+                        Color.DarkGray
+                    );
+                }
+            }
+        }
+
+    }
+}
